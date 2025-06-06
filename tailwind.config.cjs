b47f81df62b3e22bbb42e48627d30ea7dda97c7a
Armando Gonzalez <armando.gonzalez@artlogic.net>
2025-06-06 15:09:18 +0100

    big THIGNS COMING

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/**/*.{html,svelte,svx,js,ts}' // include .svx for mdsvex files
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
